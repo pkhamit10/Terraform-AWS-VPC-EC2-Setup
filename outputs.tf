95e020b210c868e1e56b9c036eaceec4740c6f29
PK <pk@PKs-MacBook-Pro.local>
2025-10-31 16:13:03 +0000

    added new values to resource

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-value = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnets" {
-value = module.vpc.public_subnet_ids
+  value = module.vpc.public_subnet_ids
 }
 
 output "ec2_public_ip" {
-value = module.ec2.public_ip
+  value = module.ec2.public_ip
 }
